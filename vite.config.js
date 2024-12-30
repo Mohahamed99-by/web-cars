9968fe94f05ad4d3cc89b178473e4cb9c7420f54
Mohahamed99-by <mohamedtolba161@gmail.com>
2024-12-30 19:09:27 +0100

    first comite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/cars/',
   server: {
     host: true, // Listen on all local IPs
     port: 8080, // Changed port to 8080
